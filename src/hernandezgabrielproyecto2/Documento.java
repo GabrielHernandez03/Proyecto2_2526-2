cc3340f4a374e42b91bb8e9bd9354c2edfc3ef36
Gabriel <GabrielHernandez03>
2026-03-20 23:27:18 -0400

    documento

---
+++ src/hernandezgabrielproyecto2/Documento.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hernandezgabrielproyecto2;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Documento {
+    String nombre;
+    int tamano;
+    String tipo;
+
+    public Documento(String nombre, int tamano, String tipo) {
+        this.nombre = nombre;
+        this.tamano = tamano;
+        this.tipo = tipo;
+    }
+}
