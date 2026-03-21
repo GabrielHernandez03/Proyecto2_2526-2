2b1481720e8048b1fa4a55ccfadbb81d3e49c84b
Gabriel <GabrielHernandez03>
2026-03-20 23:29:41 -0400

    nodolista

---
+++ src/hernandezgabrielproyecto2/NodoLista.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hernandezgabrielproyecto2;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class NodoLista {
+    Documento valor;
+    NodoLista siguiente;
+
+    public NodoLista(Documento valor) {
+        this.valor = valor;
+        this.siguiente = null;
+    }
+}
