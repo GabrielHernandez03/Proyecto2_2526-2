f58ddf2c1abadcad46789699804c70b6768e7ca4
Gabriel <GabrielHernandez03>
2026-03-22 23:17:22 -0400

    ListaUsuario

---
+++ src/hernandezgabrielproyecto2/ListaUsuarios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hernandezgabrielproyecto2;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ListaUsuarios {
+    NodoUsuario cabeza;
+    
+    public ListaUsuarios(){
+        cabeza = null;
+    }
+}
