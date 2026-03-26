e18f42ebacc9f9d0a6b1d0cf1122afb04dff615e
Gabriel <GabrielHernandez03>
2026-03-25 23:34:17 -0400

    JAVADOC REGISTROIMPRESION Y FINAL

--- src/hernandezgabrielproyecto2/RegistroImpresion.java
+++ src/hernandezgabrielproyecto2/RegistroImpresion.java
@@ -5,13 +5,18 @@
 package hernandezgabrielproyecto2;
 
 /**
- *
+ * un simple empaque que junta un documento con su etiqueta de tiempo
  * @author Gabriel
  */
 public class RegistroImpresion {
     Documento documento;
     int etiquetaTiempo;
 
+    /**
+     * crea el registro para mandarlo a la cola
+     * @param documento el archivo que se va a imprimir
+     * @param etiquetaTiempo el numero de prioridad segun el reloj
+     */
     public RegistroImpresion(Documento documento, int etiquetaTiempo) {
         this.documento = documento;
         this.etiquetaTiempo = etiquetaTiempo;
