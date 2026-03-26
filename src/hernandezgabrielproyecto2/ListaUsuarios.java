3a05923f17f928d61e5f906dbfadc5ef6b3e0242
Gabriel <GabrielHernandez03>
2026-03-25 23:33:21 -0400

    JAVADOC LISTAUSUARIOS Y FINAL

--- src/hernandezgabrielproyecto2/ListaUsuarios.java
+++ src/hernandezgabrielproyecto2/ListaUsuarios.java
@@ -5,16 +5,23 @@
 package hernandezgabrielproyecto2;
 
 /**
- *
+ * maneja el grupo de usuarios que estan registrados en la simulacion
  * @author Gabriel
  */
 public class ListaUsuarios {
     NodoUsuario cabeza;
     
+    /**
+     * inicializa la lista de usuarios basia
+     */
     public ListaUsuarios(){
         cabeza = null;
     }
     
+    /**
+     * mete a un usuario nuevo en la lista
+     * @param user el usuario a registrar
+     */
     public void agregar(Usuario user) {
         NodoUsuario nuevo = new NodoUsuario(user);
         if (cabeza == null) {
@@ -24,10 +31,14 @@ public class ListaUsuarios {
             while (actual.siguiente != null) {
                 actual = actual.siguiente;
             }
-            actual.siguiente = nuevo;
+            actual.siguiente = (nuevo);
         }
     }
     
+    /**
+     * saca a un usuario de la lista usando su nombre
+     * @param nombre el identificador del usuario a borrar
+     */
     public void eliminar(String nombre) {
         if (cabeza == null) return;
         if (cabeza.valor.nombre.equals(nombre)) {
@@ -43,6 +54,11 @@ public class ListaUsuarios {
         }
     }
     
+    /**
+     * busca un usuario por su nombre para ver si existe
+     * @param nombre el nombre que estamos buscando
+     * @return el usuario si se encontro o null si no
+     */
     public Usuario buscar(String nombre) {
         NodoUsuario actual = cabeza;
         while (actual != null) {
