8c83f87abc605217aa974bed5cd04ac97115a2e9
Gabriel <GabrielHernandez03>
2026-03-25 23:32:32 -0400

    JAVADOC DOCUMENTO Y FINAL

--- src/hernandezgabrielproyecto2/Documento.java
+++ src/hernandezgabrielproyecto2/Documento.java
@@ -5,7 +5,7 @@
 package hernandezgabrielproyecto2;
 
 /**
- *
+ * representa un documento que se quiere imprimir en el sistema
  * @author Gabriel
  */
 public class Documento {
@@ -14,6 +14,12 @@ public class Documento {
     String tipo;
     boolean enCola;
 
+    /**
+     * crea un nuevo documento con sus datos basicos
+     * @param nombre el nombre del archivo
+     * @param tamano que tan grande es el doc
+     * @param tipo el formato del archivo
+     */
     public Documento(String nombre, int tamano, String tipo) {
         this.nombre = nombre;
         this.tamano = tamano;
